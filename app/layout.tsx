1c327339b27b9370195450a1691378ab26d95af9
aureliasyrch <aureliasyrch04@gmail.com>
2026-05-02 14:07:41 +0700

    chore: add google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import Footer from "./components/Footer";
 export const metadata = {
   title: "Nayra Game Store",
   description: "Top Up Game Murah & Terpercaya",
+  // Tambahkan baris verification di bawah ini
+  verification: {
+    google: "nph-DS9mtY5L5KS5yiBo--ZUXHlxcEzoFa3lLlf2gyo",
+  },
 };
 
 export default function RootLayout({
@@ -14,7 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      {/* Ganti bg-[#D0E7FF] menjadi bg-[#0B0E14] agar seluruh sela layar berwarna hitam pekat */}
       <body className="bg-[#0B0E14] text-slate-900 antialiased min-h-screen">
         <Navbar />
         <main className="pt-16 md:pt-20">{children}</main>
